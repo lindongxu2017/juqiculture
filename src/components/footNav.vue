0d13f0faf5efcd7291af15b3552c25a7f45f21f0
lindongxu <1549482537@qq.com>
2019-04-24 17:26:25 +0800

    first commit

---
+++ src/components/footNav.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="footNav">
+        1111111
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'footNav',
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
